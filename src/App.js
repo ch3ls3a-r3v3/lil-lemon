8d64fc7b9b1b5f0ee03bcf8853c08a1f98eebf3e
ch3ls3a-r3v3 <ch3ls3a.r3v3@gmail.com>
2025-01-05 00:07:20 -0600

    updating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 import './App.css';
 
-function App() {
+const App= () => {
   return (
-    <div className="App">
+    <><Header>
+      <title>Lil Lemon Resturante
+      <meta name="og:description" content="Food"/>
+        <meta property="og:title" content="Lil Lemon" />
+        <meta property="og:type" content="cafe webpage" />
+        <meta property="og:url" content="https://www.github.com/ch3ls3a-r3v3/lil-lemon" />
+<meta property="og:image" content="https://github.com/ch3ls3a-r3v3/lil-lemon" />
+        </title>
+        <Img></Img>
+        </Header>
+    <Nav></Nav>
+    <Main>
       <p>Homepage</p>
-    </div>
+    </Main><Footer>
+      </Footer></>
   );
-}
+};
 
 export default App;
